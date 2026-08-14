ebaa95177fdeaf3d30530aa21e66e5702b671947
nooralam086 <na2209487@gmail.com>
2026-08-14 12:12:41 +0530

    code

---
+++ checkEorO.js
@@ -0,0 +1,7 @@
+let check = (num)=>{
+    if (num%2==0)
+        return "even number";
+    else
+        return "odd number";
+}
+console.log(check(10))
---
+++ sum.js
@@ -0,0 +1,6 @@
+let sum = (a , b) =>{
+    return a+b;
+    
+}
+console.log(sum(10,20));
+console.log(sum(40,20));
